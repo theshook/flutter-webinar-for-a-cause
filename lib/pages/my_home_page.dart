d32d4ca23c3073f60c4f65a48639be013cab0f49
Haji Fernandez <hjefernandez@gmail.com>
2020-11-22 16:48:20 +0800

    added dev branch

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(widget.title + 'Dev'),
       ),
       body: users.length == 0
           ? Center(
