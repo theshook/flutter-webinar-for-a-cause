ccf543d68064b06c0a669b89865e9ca7892f5a0d
Haji Fernandez <hjefernandez@gmail.com>
2020-11-22 16:56:15 +0800

    added work

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title + 'Dev'),
+        title: Text(widget.title + 'Dev Work'),
       ),
       body: users.length == 0
           ? Center(
